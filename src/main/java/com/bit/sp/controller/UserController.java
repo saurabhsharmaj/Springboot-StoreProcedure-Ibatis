cbda6c69dce9c6fecacf4bc429c1dd109a40600b
DESKTOP-0AQCN09\win 11 <ersaurabhsharma.mca@gmail.com>
2025-09-30 00:56:12 +0530

    update the export excel feature.

--- src/main/java/com/bit/sp/controller/UserController.java
+++ src/main/java/com/bit/sp/controller/UserController.java
@@ -1,6 +1,8 @@
 package com.bit.sp.controller;
 
+import java.io.IOException;
 import java.sql.Date;
+import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.web.bind.annotation.GetMapping;
@@ -9,22 +11,42 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.bit.sp.dto.UserDto;
 import com.bit.sp.service.UserService;
+import com.bit.sp.utils.ExcelExportUtil;
+
+import jakarta.servlet.http.HttpServletResponse;
 
 @RestController
 public class UserController {
+
 	private final UserService userService;
 
-	public UserController(UserService userService) {
+	private final ExcelExportUtil excelExportUtil;
+
+	public UserController(UserService userService, ExcelExportUtil excelExportUtil) {
 		this.userService = userService;
+		this.excelExportUtil = excelExportUtil;
 	}
 
 	@GetMapping("/users")
 	public List<UserDto> getUsersByStatusAndDates(@RequestParam(required = false) String status,
-			@RequestParam String startDate,
-			@RequestParam String endDate) {
+			@RequestParam String startDate, @RequestParam String endDate) {
 		if (status.equalsIgnoreCase("active"))
-			return userService.getUsersByStatusAndDatesFromProcedure(status, Date.valueOf(startDate), Date.valueOf(endDate));
+			return userService.getUsersByStatusAndDatesFromProcedure(status, Date.valueOf(startDate),
+					Date.valueOf(endDate));
 		else
 			return userService.getUsersByStatusAndDates(status, Date.valueOf(startDate), Date.valueOf(endDate));
+	}
+
+	@GetMapping("/users/excel")
+	public void getUsersByStatusAndDates(@RequestParam(required = false) String status,
+			@RequestParam String startDate, @RequestParam String endDate, HttpServletResponse response) throws IOException {
+		List<UserDto> users = new ArrayList<UserDto>();
+		if (status.equalsIgnoreCase("active"))
+			users = userService.getUsersByStatusAndDatesFromProcedure(status, Date.valueOf(startDate),
+					Date.valueOf(endDate));
+		else
+			users = userService.getUsersByStatusAndDates(status, Date.valueOf(startDate), Date.valueOf(endDate));
+
+		excelExportUtil.exportToExcel(response, users, "Users_Report");
 	}
 }
